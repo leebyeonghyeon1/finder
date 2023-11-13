1513b5133a1f74dc4e55b1759a6ecd6fbd58005e
lbh <111191932+leebyeonghyon1@users.noreply.github.com>
2023-11-13 20:16:49 +0900

    app.py's module

---
+++ logger_config.py
@@ -0,0 +1,54 @@
+import logging
+import os
+import datetime
+from werkzeug.serving import WSGIRequestHandler
+
+
+def setup_logger():
+    log_file_path = os.path.join(os.getcwd(), "app.log")
+
+    # 로거 객체 생성
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    # 파일 핸들러 설정
+    file_handler = logging.FileHandler(log_file_path)
+    file_handler.setLevel(logging.DEBUG)
+
+    # 스트림 핸들러 설정 (터미널에 출력하기 위함)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+
+    # 로깅 포맷 설정
+    log_format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+    # 파일 핸들러와 스트림 핸들러에 로깅 포맷을 적용
+    file_handler.setFormatter(log_format)
+    stream_handler.setFormatter(log_format)
+
+    # 핸들러를 로거에 추가
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+
+class CustomRequestHandler(WSGIRequestHandler):
+    def log_request(self, code="-", size="-"):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.log(
+            "info",
+            'Client %s - - [%s] "%s %s %s" %s %s',
+            self.client_address[0],
+            current_time,
+            self.command,
+            self.path,
+            self.request_version,
+            code,
+            size,
+        )
+
+
+# 요청 핸들러 설정을 위한 함수
+def set_custom_request_handler():
+    WSGIRequestHandler.log_request = CustomRequestHandler.log_request.__get__(
+        CustomRequestHandler
+    )
---
+++ word_map.py
@@ -0,0 +1,13 @@
+# 쿼리 번역 딕셔너리
+word_mapping = {
+    "신라면 컵": "Shin Ramyun -cup- 65g",
+    "새우탕 컵": "Shrimp soup noodle -cup- 67g",
+    "짜파구리 컵": "Angry Chapaguri -cup- 108 g",
+    "쇠고기 미역국라면 컵": "beef and seaweed soup -cup- 100g",
+    "불닭볶음면 팩": "Hot Chicken Flavor Ramen 140g",
+    "진라면 컵": "Jin Ramen Spicy -Cup- 65g",
+    "진라면 팩": "Jin Ramen Spicy 120g",
+    "참깨라면 컵": "Chamggae Ramen -cup- 65g",
+    "안성탕면 팩": "Anseongtangmyeon -cup- 125g",
+    "짜파게티 컵": "Chapagetti -cup- 123g",
+}
